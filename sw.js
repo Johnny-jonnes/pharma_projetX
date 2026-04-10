4c1e97c874cc67d19c2e54b1fca61a424d5771b8
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-04-10 16:38:47 +0000

    chore: nettoyage des logs de debug et passage en version 5.3.0-stable

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v5.2.0-fix-cache';
+const CACHE_NAME = 'pharma-cache-v5.3.0-stable';
 const ASSETS = [
   './',
   './index.html',
