ab60ba562f0db0b14e4f1a08c8055c84e908d899
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-04-11 07:57:25 +0000

    fix: pad bottom of mobile modals to prevent overlapping with the fixed tab bar

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v6.3.6';
+const CACHE_NAME = 'pharma-cache-v6.3.7';
 const ASSETS = [
   './',
   './index.html',
