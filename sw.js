e60f1cd293b383098fd6a6a65e95231000e2cae3
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-04-11 08:23:28 +0000

    fix: force le navigateur à télécharger la nouvelle version de db.js et sw.js (cache burst) pour appliquer l\'intercepteur offline

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v6.4.0';
+const CACHE_NAME = 'pharma-cache-v6.4.1';
 const ASSETS = [
   './',
   './index.html',
